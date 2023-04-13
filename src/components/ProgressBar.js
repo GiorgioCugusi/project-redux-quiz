09c162e5390c39642defbdd9f3286face9a42a95
malvinamaria <mmpolak@protonmail.com>
2023-04-13 16:26:16 +0200

    add progress bar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,19 +1,31 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+// useSelector hook is used to access the Redux store and retrieve data from it.
 import { useSelector } from 'react-redux';
 import './ProgressBar.css';
 
+// functional component called ProgressBar which is exported
+//  to be used in other parts of the application.
 export const ProgressBar = () => {
+  // useSelector hook retrieves the questions array from redux store
+  // It takes a function as an argument that returns a specific piece of state from the store.
   const questions = useSelector((state) => state.quiz.questions);
+  // useSelector hook retrieves the currentQuestionIndex from redux store
   const currentQuestionIndex = useSelector((state) => {
     return state.quiz.questions[state.quiz.currentQuestionIndex];
   });
 
   return (
-    <div className="container">
-      <h1>
+    <div className="progress-element">
+      <p className="progress-label">
         Question: {currentQuestionIndex.id} / {questions.length}
-      </h1>
+      </p>
+      <div className="container">
+        {/* <progress value="20" max="100"></progress> */}
+        <h1>
+          Question: {currentQuestionIndex.id} / {questions.length}
+        </h1>
+      </div>
     </div>
   );
 };
